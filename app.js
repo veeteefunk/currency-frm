a3e00476dbe09b926e0298642cc26f11af983eb2
Victor Marin <victormarinviloria@gmail.com>
2022-03-18 07:28:13 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
-let btn = document.getElementById("btn");
-let rupees = document.getElementById("output1");
-let dollars = document.getElementById("output2");
-let yen = document.getElementById("output3");
+const password = document.querySelector("#password");
+const warning = document.querySelector("#warning");
 
-let formatter1 = new Intl.NumberFormat(
-    "en-IN",
-    {style: "currency", currency: "INR"}
-);
-let formatter2 = new Intl.NumberFormat(
-    "en-US",
-    {style: "currency", currency: "USD"}
-);
-let formatter3 = new Intl.NumberFormat(
-    "ja-Jp",
-    {style: "currency", currency: "JPY"}
-);
-
-btn.addEventListener("click", ()=> {
-    let amount = document.getElementById("amount").value;
-
-    rupees.innerHTML = `<span>Indian Rupees:</span>
-    ${formatter1.format(amount)}`;
-    dollars.innerHTML = `<span>US Dollars:</span>
-    ${formatter2.format(amount)}`;
-    yen.innerHTML = `<span>Japanese Yen:</span>
-    ${formatter3.format(amount)}`;
-    
+password.addEventListener("keyup", function(e) {
+    if(e.getModifierState("CapsLock")){
+        warning.style.display = "block";
+    }
+    else {
+        warning.style.display = "none";
+    }
 })
